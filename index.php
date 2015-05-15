ea752f33104f004a65a76196c56c440e5d1caf41
ethender <ethender@hotmail.com>
2015-05-15 18:13:34 +0530

    index full added menu

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     <head>
         <title>PizzaML</title>
     <head>
-    <body>
-        <form action="form.php" method="get">
+    <body background="#D0D0D0">
+        <form action="index.php" method="get">
             <table align="center" border=1>
                 <tr>
                     <th>Pizzas</th>
@@ -15,20 +15,79 @@
             <?
                 $dom = simplexml_load_file("menu.xml");
 
+                    //<!-- pizzas category -->
                 foreach($dom -> xpath("category[@name='Pizzas']/item") as $item){
                             print "<tr>";
                             print"<td>";
                             print $item["name"];
                             print"</td>";
                             print"<td>";
-                            print "<input type=radio value=$item[small]>"."small ";
+                            print "<input type=checkbox value=$item[small]>".$item["small"];
                             print "</td>";
                             print "<td>";
-                            print "<input type=radio value=$item[small]>"."large ";
+                            print "<input type=checkbox value=$item[small]>".$item["large"];
                             print "</td>";
                             print "</tr>";
     
                 }
+
+                // <!-- special catgory  -->
+
+                        print"<tr> <th>Special Pizza </th> <th> small </th> <th> large </th> </tr>";
+
+                        foreach($dom -> xpath("category[@name='SpecialityPizzas']/item") as $item){
+                                print "<tr>";
+                                print"<td>";
+                                print $item["name"];
+                                print"</td>";
+                                print"<td>";
+                                print "<input type=checkbox value=$item[small]>".$item["small"];
+                                print "</td>";
+                                print "<td>";
+                                print "<input type=checkbox value=$item[small]>".$item["large"];
+                                print "</td>";
+                                print "</tr>";
+    
+                        }
+            //<!-- salads category -->
+
+                print"<tr> <th>Special Pizza </th> <th> small </th> <th> large </th> </tr>";
+
+                foreach($dom -> xpath("category[@name='Salads']/item") as $item){
+                        print "<tr>";
+                        print"<td>";
+                        print $item["name"];
+                        print"</td>";
+                        print"<td>";
+                        print "<input type=checkbox value=$item[small]>".$item["small"];
+                        print "</td>";
+                        print "<td>";
+                        print "<input type=checkbox value=$item[small]>".$item["large"];
+                        print "</td>";
+                        print "</tr>";
+    
+                }
+        //<!-- Grinders -->
+
+            print"<tr> <th>Special Pizza </th> <th> small </th> <th> large </th> </tr>";
+
+            foreach($dom -> xpath("category[@name='Grinders']/item") as $item){
+                    print "<tr>";
+                    print"<td>";
+                    print $item["name"];
+                    print"</td>";
+                    print"<td>";
+                    print "<input type=checkbox value=$item[small]>".$item["small"];
+                    print "</td>";
+                    print "<td>";
+                    print "<input type=checkbox value=$item[small]>".$item["large"];
+                    print "</td>";
+                    print "</tr>";
+            }
+
+
+                    //<!-- submit button -->
+
                             print "<tr>";
                             print "<td>";
                             print "<input type=submit value=submit>";
